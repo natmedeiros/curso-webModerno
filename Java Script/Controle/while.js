8031d32a7cb7bfc87c33d492148e88addcde537d
Natalia <nataliamaria868@gmail.com>
2023-08-17 23:22:11 -0300

    Complemento

---
+++ Java Script/Controle/while.js
@@ -0,0 +1,15 @@
+// a estrura while é usada para laços de repetição
+
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while (opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+}
+
+console.log('Até a próxima!')
---
+++ Java Script/Fundamentos/organizacao.js
@@ -0,0 +1,12 @@
+console.log("Sentença de código");
+
+{
+    {
+        console.log("Olá");
+        console.log('Mundo!');
+    }
+}
+
+/* O JavaScript é organizado com senteças de código que podem
+ou não terminar com ";". E também é organizado com blocos de códigos, delimitados
+por "{}". */
