e2b567c5e1bd34d1274c7a4ca27837a0a6290a63
Natalia Medeiros <nataliamaria868@gmail.com>
2024-03-13 00:12:21 -0300

    Complemento

---
+++ Java Script/Funcao/tiposDeclaracao.js
@@ -0,0 +1,19 @@
+console.log(soma(2, 3))
+
+// Function declaracion
+function soma(x, y) {
+    return x + y
+}
+
+// Function expression
+const sub = function (x, y) {
+    return x - y
+}
+console.log(sub(5, 3))
+
+// Named function expression
+const mult = function mullt(x, y) {
+    return x * y
+}
+
+console.log(mult(5, 7))
